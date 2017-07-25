b3274b8fdfcd6842fcaff4ca6b7d2f9d61fed0e0
cmitch024 <cmitch024@gmail.com>
2017-07-25 12:49:19 -0500

    Added UIImage view and a toolbar with a button.  The view and the button are wired to the ViewController and the .plist file has the necessary comments to access the photo library.

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -10,16 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var memeImage: UIImageView!
+    
+    @IBAction func pickAnImage(_ sender: Any) {
+        let pickerController = UIImagePickerController()
+        present(pickerController, animated: true, completion: nil)
+    } // end pickAnImage
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
-}
+    } // end viewDidLoad
+    
+} // end ViewController
 
