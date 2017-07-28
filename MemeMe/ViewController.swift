b7ce1575aae6c93b3f2c442b76c982d901d8c495
cmitch024 <cmitch024@gmail.com>
2017-07-27 22:30:45 -0500

    Created a UIImagePickerDelegate object and set it as delegate.  Improved functionality of pick button.  The button now sets the UIImage to the selected photo.

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -8,19 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
 
     @IBOutlet weak var memeImage: UIImageView!
     
     @IBAction func pickAnImage(_ sender: Any) {
-        let pickerController = UIImagePickerController()
-        present(pickerController, animated: true, completion: nil)
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        present(imagePicker, animated: true, completion: nil)
     } // end pickAnImage
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     } // end viewDidLoad
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        print("imagePickerController method called.")
+        
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            memeImage.image = image
+        }
+        
+        dismiss(animated: true, completion: nil)
+    } // end imagePickerController
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        print("imagePickerControllerDidCancel method called.")
+        dismiss(animated: true, completion: nil)
+    } // end imagePickerControllerDidCancel
     
 } // end ViewController
 
